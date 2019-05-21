2092c8709b9a2c1f53c448c5a18eac19ef03b0c5
nevola <laura.garcia@zevenet.com>
2019-05-21 12:01:15 +0200

    farms: avoid to reload the farm is the state is changed
    This patch avoids to stop an already stopped farm which
    can produce execution rules errors.
    
    Signed-off-by: nevola <laura.garcia@zevenet.com>

--- src/farms.c
+++ src/farms.c
@@ -583,6 +583,7 @@ int farm_pos_actionable(struct config_pair *c)
 		farm_set_action(f, ACTION_START);
 		break;
 	case KEY_STATE:
+		break;
 	default:
 		farm_set_action(f, ACTION_RELOAD);
 		return 0;
